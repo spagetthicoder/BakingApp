5797aed44cfe61995c07540cd6a5300d17e924c9
n3o <proboyski@gmail.com>
2018-08-28 17:36:09 +0300

    Espresso Test

---
+++ app/src/androidTest/java/com/example/android/bakingapp/MainActivityBasicTest.java
@@ -0,0 +1,47 @@
+package com.example.android.bakingapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityBasicTest {
+    public static final String CAKE_NAME = "Nutella Pie";
+
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested will be launched before each test that's annotated
+     * with @Test and before methods annotated with @Before. The activity will be terminated after
+     * the test and methods annotated with @After are complete. This rule allows you to directly
+     * access the activity during the test.
+     */
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * Clicks on a GridView item and checks it opens up the Main with the correct details.
+     */
+    @Test
+    public void clickGridViewItem_OpensOrderActivity() {
+
+        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
+        // gridview item and clicks it.
+        onData(anything()).inAdapterView(withId(R.id.recipe_recycler)).atPosition(1).perform(click());
+
+        // Checks that the OrderActivity opens with the correct tea name displayed
+        onView(withId(R.id.recipe_recycler)).check(matches(withText(CAKE_NAME)));
+
+    }
+}
