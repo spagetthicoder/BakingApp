bafd90bffd4bff5baa499136059517e12f5d3a9f
n3o <proboyski@gmail.com>
2018-08-29 17:31:02 +0300

    Espresso Test added

--- app/src/androidTest/java/com/example/android/bakingapp/RecipeDetaileActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/bakingapp/RecipeDetaileActivityBasicTest.java
@@ -1,11 +1,9 @@
 package com.example.android.bakingapp;
 
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -15,10 +13,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anything;
 
-
-@RunWith(AndroidJUnit4.class)
-public class MainActivityBasicTest {
-    public static final String CAKE_NAME = "Nutella Pie";
+public class RecipeDetaileActivityBasicTest {
+    public static final String STEP_NAME = "0. Recipe Introduction";
 
     /**
      * The ActivityTestRule is a rule provided by Android used for functional testing of a single
@@ -28,20 +24,20 @@ public class MainActivityBasicTest {
      * access the activity during the test.
      */
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<RecipeDetailActivity> mActivityTestRule = new ActivityTestRule<>(RecipeDetailActivity.class);
 
     /**
      * Clicks on a GridView item and checks it opens up the Main with the correct details.
      */
     @Test
-    public void clickGridViewItem_OpensMainActivity() {
+    public void clickGridViewItem_OpensRecipeDetailActivity() {
 
         // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
         // gridview item and clicks it.
-        onData(anything()).inAdapterView(withId(R.id.recipe_recycler)).atPosition(0).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.recipe_detail_recycler)).atPosition(0).perform(click());
 
-        // Checks that the MainActivity opens with the correct recipe name displayed
-        onView(withId(R.id.recipe_recycler)).check(matches(withText(CAKE_NAME)));
+        // Checks that the RecipeDetailActivity opens with the correct recipe name displayed
+        onView(withId(R.id.recipe_detail_recycler)).check(matches(withText(STEP_NAME)));
 
     }
 }
